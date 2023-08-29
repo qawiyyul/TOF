6238df572cdec640d1e00a6aa8eb5579b8ffa2bd
Qawiyyul <md.qawiyyul.ameen@gmail.com>
2023-08-29 15:11:22 +0800

    added script for ros node (incomplete)

--- SerialRead.py
+++ SerialRead.py
@@ -11,36 +11,44 @@ im=plt.imshow(arr2D,cmap='RdYlBu')
 plt.colorbar()
 print("Starting \n")
 #plt.pause(1)
-while(1):
+def main():
+    while(1):
     
-    # Wait until there is data waiting in the serial buffer
-    if(serialPort.in_waiting > 0):
-        #print("Printing \n")
-        # Read data out of the buffer until a carraige return / new line is found
-        serialString = serialPort.readline()
+        # Wait until there is data waiting in the serial buffer
+        if(serialPort.in_waiting > 0):
+            #print("Printing \n")
+            # Read data out of the buffer until a carraige return / new line is found
+            serialString = serialPort.readline()
 
-        # Print the contents of the serial data
-        #print(serialString,"\n")
-        
-        #translate the serial message into numpy array
-        list1D=[]
-        list2D=[]
-        for i in range(0,256,32):
-            for j in range(0,32,4):
-                list1D.append(int(serialString[i+j:i+j+4]))
-            list2D.append(list1D)
+            # Print the contents of the serial data
+            #print(serialString,"\n")
+            
+            #translate the serial message into numpy array
             list1D=[]
-        arr2D=np.array(list2D)
-        
-        #interpolate to higher resolution
-        new_size=(32,32)
+            list2D=[]
+            for i in range(0,256,32):
+                for j in range(0,32,4):
+                    list1D.append(int(serialString[i+j:i+j+4]))
+                list2D.append(list1D)
+                list1D=[]
+            arr2D=np.array(list2D)
+            
+            #interpolate to higher resolution
+            arr2D=array2D_interpolate(arr2D,32)
+            
+            #Update Colormesh
+            im.set_data(arr2D)
+            #print(arr2D)
+            plt.pause(0.01)
+
+
+def array2D_interpolate(array2D,resolution):
+        new_size=(resolution,resolution)
         new_dims = []
         for original_length, new_length in zip(arr2D.shape, new_size):
             new_dims.append(np.linspace(0, original_length-1, new_length))
         coords = np.meshgrid(*new_dims, indexing='ij')
-        arr2D=map_coordinates(arr2D,coords)
-        
-        #Update Colormesh
-        im.set_data(arr2D)
-        #print(arr2D)
-        plt.pause(0.01)
+        return map_coordinates(array2D,coords)
+
+if __name__ == '__main__':
+    main()
